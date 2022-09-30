62948be2038e47b54aa01ab9c97abb85e8dd3943
Gerardo Martínez <gerardomt@ciencias.unam.mx>
2022-09-30 17:53:58 -0500

    Completed first version of cli with argparse

--- app/main.py
+++ app/main.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
 
 import sys
+import argparse
 from draw_cistercian import DrawCistercian
+
+# Configuration of the parser
+parser = argparse.ArgumentParser(
+    description="Creates a svg image of the integer between 0 and 9999",
+    prog="cistercian_cli")
+parser.add_argument("number", type=int, help="Integer number that will be draw")
+parser.add_argument("-f", "--filename", default="out.svg",
+                    help="Filename of the SVG file that will be created")
+parser.add_argument("--width", type=int, default=480,
+                    help="Width of the SVG image in pixels")
+parser.add_argument("--height", type=int, default=720,
+                    help="Height of the SVG image in pixels")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Displays more information")
 
 
 def main():
-    if len(sys.argv) == 3:
-        try:
-            n = int(sys.argv[1])
-        except ValueError:
-            print("Error: The argument is not an integer.")
+    args = parser.parse_args()
+    if 0 > args.number > 9999:
+        print("Error: The number must be an integer between 0 and 9999")
+        sys.exit(1)
+    if args.width <= 0 or args.height <= 0:
+        print("Error: The width and height of the file must be positive")
+        sys.exit(1)
 
-        d = DrawCistercian(480, 720)
-        d.draw_number(n)
-        d.save(sys.argv[2])
-    else:
-        print("Error: You must pass an integer an the name of the file")
+    d = DrawCistercian(args.width, args.height)
+    # TODO: add stroke size and colors in the options
+    d.draw_number(args.number)
+    # TODO: make sure that the filename ends with .svg
+    d.save(args.filename)
+
+    if args.verbose:
+        print(f"Created image {args.filename} with the cistercian number {args.number}")
 
 
 if __name__ == "__main__":
